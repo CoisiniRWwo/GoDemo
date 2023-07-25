bf693fd9bac7285a78e78baa116136bec57ca12e
CoisiniRWwo <1328234978@qq.com>
2023-07-25 22:11:30 +0800

    Initial commit

---
+++ Demo02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("你好 goland!")
+	fmt.Println("你好 goland")
+
+	var a = "aaa"
+	println(a)
+
+	b := 30
+	c := 2
+	fmt.Printf("b=%v c=%v", b, c)
+}
