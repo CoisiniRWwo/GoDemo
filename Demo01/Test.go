ed1e0f6b371c14bbda56bc0e0edce6a32a571c3a
CoisiniRWwo <1328234978@qq.com>
2023-07-31 20:49:56 +0800

    golang中空指针和类型断言使用细节

--- Demo01/Test.go
+++ Demo01/Test.go
@@ -1,11 +1,32 @@
-//package main
-//
-//import (
-//	"fmt"
-//	"itying/calc"
-//)
-//
-//func main() {
-//	c := calc.Add(10, 20)
-//	fmt.Println(c)
-//}
+package main
+
+import "fmt"
+
+type SayInterface interface {
+	say()
+}
+type MoveInterface interface {
+	move()
+}
+
+// 接口嵌套
+type Animal interface {
+	SayInterface
+	MoveInterface
+}
+type Cat struct {
+	name string
+}
+
+func (c Cat) say() {
+	fmt.Println("喵喵喵")
+}
+func (c Cat) move() {
+	fmt.Println("猫会动")
+}
+func main() {
+	var x Animal
+	x = Cat{name: "花花"}
+	x.move()
+	x.say()
+}
---
+++ Demo07/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	Name  string
+	Phone int
+}
+
+// golang中空指针和类型断言使用细节
+func main() {
+	userinfo := make(map[string]interface{})
+	userinfo["username"] = "张三"
+	userinfo["age"] = 22
+	userinfo["hobby"] = []string{"敲代码", "看番"}
+
+	fmt.Println(userinfo["username"])
+	fmt.Println(userinfo["age"])
+	assertHobby, _ := userinfo["hobby"].([]string)
+	fmt.Println(assertHobby[0])
+
+	address := new(Address)
+	address.Name = "李四"
+	address.Phone = 110
+
+	fmt.Println(address)
+
+	userinfo["address"] = *address
+	assertAddress, _ := userinfo["address"].(Address)
+	fmt.Println(assertAddress.Name, assertAddress.Phone)
+}
