805f036e7e6ff47bfee9937af021114f541edb29
CoisiniRWwo <1328234978@qq.com>
2023-08-07 23:29:45 +0800

    gin05

--- Demo01/Test.go
+++ Demo01/Test.go
@@ -3,22 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
-func formatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
-}
-func main() {
-	router := gin.Default()
-	router.GET("/addUser", func(c *gin.Context) {
-		c.HTML(200, "default/add_user.html", gin.H{})
-	})
-	router.POST("/doAddUser", func(c *gin.Context) {
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-		age := c.DefaultPostForm("age", "20")
-		c.JSON(200, gin.H{"usernmae": username, "password": password, "age": age})
-	})
+func InitAdminMiddleware(ctx *gin.Context) {
+	fmt.Println("路由分组中间件")
+	// 可以通过 ctx.Set 在请求上下文中设置值，后续的处理函数能够取到该值ctx.Set("username", "张三")
+	// 调用该请求的剩余处理程序
+	ctx.Next()
 }
